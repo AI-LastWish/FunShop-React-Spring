cd971399db9a011e3cbd22388742c0138015b17b
Nguyen Quang Tri <nguyenquangtri@Nguyens-MacBook-Air.local>
2020-10-11 21:43:18 +0700

    5. Implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">FunShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>FunShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart' />Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user' />Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart' />Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link><i className='fas fa-user' />Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
