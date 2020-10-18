0a00723cbfafdb2e74d601cec0b0f5314d492248
Nguyen Quang Tri <nguyenquangtri@Nguyens-MacBook-Air.local>
2020-10-18 16:08:51 +0700

    7. Token Generatedgit add -A

--- backend/src/main/java/funShop/controller/UserController.java
+++ backend/src/main/java/funShop/controller/UserController.java
@@ -5,6 +5,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,26 +16,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import funShop.domain.User;
+import funShop.payload.JWTLoginSucessReponse;
+import funShop.payload.LoginRequest;
+import funShop.security.JwtTokenProvider;
 import funShop.services.impl.UserCommandService;
 import funShop.services.mapValidation.MapValidationErrorService;
 import funShop.validator.UserValidator;
+import static funShop.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
-    @Autowired
-    private MapValidationErrorService mapValidationErrorService;
-	
-    @Autowired
-    private UserValidator userValidator;
-    
-    @Autowired
-    private UserCommandService userCommandService;
+
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+
+	@Autowired
+	private UserValidator userValidator;
+
+	@Autowired
+	private UserCommandService userCommandService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
 
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) throws Exception {
-		
+
 		// Validate passwords match
 		userValidator.validate(user, result);
 
@@ -42,6 +56,23 @@ public class UserController {
 		User newUser = userCommandService.saveUser(user);
 
 		return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if (errorMap != null)
+			return errorMap;
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(
+						loginRequest.getUsername(), 
+						loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+		return ResponseEntity.ok(new JWTLoginSucessReponse(true, jwt));
 	}
 
 }
--- backend/src/main/java/funShop/exceptions/ApiExceptionHandler.java
+++ backend/src/main/java/funShop/exceptions/ApiExceptionHandler.java
@@ -1,16 +1,34 @@
 package funShop.exceptions;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import funShop.security.JwtAuthenticationEntryPoint;
+
 @RestControllerAdvice
 public class ApiExceptionHandler {
-	
+
+	@ExceptionHandler(BadCredentialsException.class)
+	@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+	public void handleUnauthorizedRequest(HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+		JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint = new JwtAuthenticationEntryPoint();
+		jwtAuthenticationEntryPoint.commence(httpServletRequest, httpServletResponse, e);
+	}
+
 	@ExceptionHandler(Exception.class)
-	@ResponseStatus(value=HttpStatus.BAD_REQUEST)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 	public ErrorMessage handleAllException(Exception ex, WebRequest request) {
 		return new ErrorMessage(ex.getLocalizedMessage());
 	}
